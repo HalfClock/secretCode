c27d9d2d81f95a4b104af08e6ec1cb65625eedac
ZhangHT <31841749+zhang99969@users.noreply.github.com>
2019-03-19 13:42:29 +0800

    Add files via upload

--- HuaweiCar.cpp
+++ HuaweiCar.cpp
@@ -474,7 +474,7 @@ int findRoad(int carid)
 	cout << endl;
 	return 0;
 }
-//398行到541行没有用到
+//398行到477行没有用到
 
 
 void TestCase1()
@@ -545,25 +545,25 @@ void TestCase1()
 
 // 各数组都从下标1开始
 float dist[maxnum];     // 表示当前点到源点的最短路径长度
-float prevww[maxnum];     // 记录当前点的前一个结点
+int prew[maxnum];     // 记录当前点的前一个结点
 
-// n -- n nodes
-// v -- the source node
+
 // dist[] -- the distance from the ith node to the source node
 // prev[] -- the previous node of the ith node
-// c[][] -- every two nodes' distance
 // 抄自https://www.cnblogs.com/simuhunluo/p/7469495.html
-void Dijkstra(int n, int v, float *dist, float *prev, float c[maxnum][maxnum])
+void Dijkstra(int Carid, float *dist, int *prev)
+/*输入Car的id，两个数组*/
 {
+	int v = findCarFrom(Carid);//v是车的起点
 	bool s[maxnum];    // 判断是否已存入该点到S集合中
-	for (int i = 1; i <= n; ++i)
+	for (int i = 1; i <= CrossNum; ++i)
 	{
-		dist[i] = c[v][i];
+		dist[i] = map[v][i];
 		s[i] = 0;     // 初始都未用过该点
 		if (dist[i] == maxint)
 			prev[i] = 0;
 		else
-			prev[i] = (float)v;
+			prev[i] = v;
 	}
 	dist[v] = 0;
 	s[v] = 1;
@@ -571,12 +571,12 @@ void Dijkstra(int n, int v, float *dist, float *prev, float c[maxnum][maxnum])
 	// 依次将未放入S集合的结点中，取dist[]最小值的结点，放入结合S中
 	// 一旦S包含了所有V中顶点，dist就记录了从源点到所有其他顶点之间的最短路径长度
 		 // 注意是从第二个节点开始，第一个为源点
-	for (int i = 2; i <= n; ++i)
+	for (int i = 2; i <= CrossNum; ++i)
 	{
 		float tmp = maxint;
 		int u = v;
 		// 找出当前未使用的点j的dist[j]最小值
-		for (int j = 1; j <= n; ++j)
+		for (int j = 1; j <= CrossNum; ++j)
 			if ((!s[j]) && dist[j] < tmp)
 			{
 				u = j;              // u保存当前邻接点中距离最小的点的号码
@@ -585,21 +585,21 @@ void Dijkstra(int n, int v, float *dist, float *prev, float c[maxnum][maxnum])
 		s[u] = 1;    // 表示u点已存入S集合中
 
 		// 更新dist
-		for (int j = 1; j <= n; ++j)
-			if ((!s[j]) && c[u][j] < maxint)
+		for (int j = 1; j <= CrossNum; ++j)
+			if ((!s[j]) && map[u][j] < maxint)
 			{
-				float newdist = dist[u] + c[u][j];
+				float newdist = dist[u] + map[u][j];
 				if (newdist < dist[j])
 				{
 					dist[j] = newdist;
-					prev[j] = (float) u;
+					prev[j] = u;
 				}
 			}
 	}
 }
 
 // 查找从源点v到终点u的路径，并输出
-void searchPath(float *prev, int v, int u)
+void searchPath(int *prev, int v, int u)
 {
 	int que[maxnum];
 	int tot = 1;
@@ -615,7 +615,7 @@ void searchPath(float *prev, int v, int u)
 	que[tot] = v;
 	for (int i = tot; i >= 1; --i)
 		if (i != 1)
-			cout << que[i] << " -> ";
+			cout << que[i] << ",";
 		else
 			cout << que[i] << endl;
 }
@@ -624,22 +624,22 @@ void clearPrevAndDist()
 	for (int i = 0; i < maxnum; i++)
 	{
 		dist[i]=0.0;
-		prevww[i]=0.0;     // 记录当前点的前一个结点
+		prew[i]=0;     // 清空两个节点
 
 	}
 }
 void BuildMapandFindRoadForCar(int Carid)
 {
-	//为1001车build地图
+	//为id为Carid的车build地图
 	buildMap(Carid);
 	//printMap();
 	for (int i = 1; i <= CrossNum; ++i)
 		dist[i] = maxint;
 
-	Dijkstra(CrossNum, findCarFrom(Carid), dist, prevww, map);
+	Dijkstra(Carid, dist, prew);
 	cout << "车辆" << Carid << "最短时间: " << dist[findCarEnd(Carid)] << endl;
 	cout << findCarFrom(Carid)<<"到"<< findCarEnd(Carid)<<"的路径为: ";
-	searchPath(prevww, findCarFrom(Carid), findCarEnd(Carid));
+	searchPath(prew, findCarFrom(Carid), findCarEnd(Carid));
 
 
 }
